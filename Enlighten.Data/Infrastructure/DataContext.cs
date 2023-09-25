0edf290ed412e27138505b01e18321aa57db9506
Jeffrey Lefebvre <j.lefebvre@uwinnipeg.ca>
2023-09-25 16:24:37 -0500

    added detached dbContext

--- Enlighten.Data/Infrastructure/DataContext.cs
+++ Enlighten.Data/Infrastructure/DataContext.cs
@@ -4,75 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Enlighten.Data.Infrastructure
 {
-    public class DataContext : DbContext
+    public class DataContext : DetachedDbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            //required LocalDB - https://learn.microsoft.com/en-us/sql/database-engine/configure-windows/sql-server-express-localdb?view=sql-server-ver16#install-localdb
-            //dotnet-ef migrations add [SUMMARY] --project Enlighten.Data --startup-project Enlighten.Study.Web
-            //dotnet-ef database update --project Enlighten.Data --startup-project Enlighten.Study.Web
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-
-            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+          
         }
 
         public DbSet<Textbook> Textbooks { get; set; }
         public DbSet<TextbookUnit> TextbookUnits { get; set; }
         public DbSet<GptDataSettingsModel> GptDataSettings { get; set; }
-
-
-        public void AddEntity<T>(T entity) where T : class
-        {
-            if (entity == null)
-                throw new ArgumentNullException(nameof(entity));
-
-            Set<T>().Add(entity);
-        }
-
-        public void UpdateEntity<T>(T entity) where T : class
-        {
-            if (entity == null)
-                throw new ArgumentNullException(nameof(entity));
-
-            Set<T>().Attach(entity);
-            Entry(entity).State = EntityState.Modified;
-        }
-
-        public void DeleteEntity<T>(T entity) where T : class
-        {
-            if (entity == null)
-                throw new ArgumentNullException(nameof(entity));
-
-            Set<T>().Remove(entity);
-        }
-
-        public override int SaveChanges()
-        {
-            var result = base.SaveChanges();
-            Reset();  // or Clear(), Complete(), FinalizeUoW(), or whatever name you chose
-            return result;
-        }
-
-        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-        {
-            var result = await base.SaveChangesAsync(cancellationToken);
-            Reset();  // or Clear(), Complete(), FinalizeUoW(), or whatever name you chose
-            return result;
-        }
-
-        public void Reset()
-        {
-            var entries = ChangeTracker.Entries().ToList();
-            foreach (var entry in entries)
-            {
-                entry.State = EntityState.Detached;
-            }
-        }
-
 
     }
 }
---
+++ Enlighten.Data/Infrastructure/DetachedDbContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enlighten.Data.Infrastructure
+{
+    public class DetachedDbContext : DbContext
+    {
+        public DetachedDbContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public void AddEntity<T>(T entity) where T : class
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            Set<T>().Add(entity);
+        }
+
+        public void UpdateEntity<T>(T entity) where T : class
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            Set<T>().Attach(entity);
+            Entry(entity).State = EntityState.Modified;
+        }
+
+        public void DeleteEntity<T>(T entity) where T : class
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            Set<T>().Remove(entity);
+        }
+
+        public override int SaveChanges()
+        {
+            var result = base.SaveChanges();
+            Reset();  // or Clear(), Complete(), FinalizeUoW(), or whatever name you chose
+            return result;
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+            Reset();  // or Clear(), Complete(), FinalizeUoW(), or whatever name you chose
+            return result;
+        }
+
+        public void Reset()
+        {
+            var entries = ChangeTracker.Entries().ToList();
+            foreach (var entry in entries)
+            {
+                entry.State = EntityState.Detached;
+            }
+        }
+    }
+}
