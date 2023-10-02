033365b68cf176f4a1fe230dfe4337f10d199e94
Jeffrey Lefebvre <j.lefebvre@uwinnipeg.ca>
2023-10-02 00:16:59 -0500

    gpt settings now get appended instead of overwritten.

--- Enlighten.Core/Services/GptPromptService.cs
+++ Enlighten.Core/Services/GptPromptService.cs
@@ -39,37 +39,37 @@ namespace Enlighten.Core.Services
             {
                 if (!string.IsNullOrEmpty(newModel.QuizSystemMessage))
                 {
-                    result.QuizSystemMessage = newModel.QuizSystemMessage;
+                    result.QuizSystemMessage += newModel.QuizSystemMessage;
                 }
 
                 if (!string.IsNullOrEmpty(newModel.QuizQuestionPrompt))
                 {
-                    result.QuizQuestionPrompt = newModel.QuizQuestionPrompt;
+                    result.QuizQuestionPrompt += newModel.QuizQuestionPrompt;
                 }
 
                 if (!string.IsNullOrEmpty(newModel.QuizAnswerPrompt))
                 {
-                    result.QuizAnswerPrompt = newModel.QuizAnswerPrompt;
+                    result.QuizAnswerPrompt += newModel.QuizAnswerPrompt;
                 }
 
                 if (!string.IsNullOrEmpty(newModel.InquireSystemMessage))
                 {
-                    result.InquireSystemMessage = newModel.InquireSystemMessage;
+                    result.InquireSystemMessage += newModel.InquireSystemMessage;
                 }
 
                 if (!string.IsNullOrEmpty(newModel.InquirePrompt))
                 {
-                    result.InquirePrompt = newModel.InquirePrompt;
+                    result.InquirePrompt += newModel.InquirePrompt;
                 }
 
                 if (!string.IsNullOrEmpty(newModel.ContentStart))
                 {
-                    result.ContentStart = newModel.ContentStart;
+                    result.ContentStart += newModel.ContentStart;
                 }
 
                 if (!string.IsNullOrEmpty(newModel.ContentEnd))
                 {
-                    result.ContentEnd = newModel.ContentEnd;
+                    result.ContentEnd += newModel.ContentEnd;
                 }
             }
 
