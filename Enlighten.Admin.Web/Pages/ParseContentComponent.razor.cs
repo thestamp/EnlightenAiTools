fa87c9a250b820513c10c8e00db850000cda64f6
Jeffrey Lefebvre <j.lefebvre@uwinnipeg.ca>
2023-10-06 17:02:52 -0500

    added content component and studycomponentservice

---
+++ Enlighten.Admin.Web/Pages/ParseContentComponent.razor.cs
@@ -0,0 +1,116 @@
+﻿using Enlighten.Core.Services;
+using Enlighten.Data.Models;
+using Enlighten.Gpt.Client.Configuration;
+using Enlighten.Study.Core.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Enlighten.Admin.Web.Pages
+{
+    public class ParseContentComponentBase : ComponentBase
+    {
+        [Inject] public GptClientSettingsModel GptClientSettingsModel { get; set; }
+        [Inject] public GptPromptService GptPromptService { get; set; }
+        [Parameter]
+        public string ResultContent { get; set; }
+        [Parameter]
+        public string ResultSummary { get; set; }
+        [Parameter]
+        public string ResultTopicList { get; set; }
+
+  
+
+        public string ViewContent = string.Empty;
+        public string ViewSummary = string.Empty;
+        public string ViewTopics = string.Empty;
+        
+
+        public string errorMessage = string.Empty;
+        private bool _processing;
+        private StudyContentService svc;
+
+        protected override async Task OnInitializedAsync()
+        {
+            svc = new StudyContentService(GptClientSettingsModel);
+
+        }
+
+        public async Task LoadFile(InputFileChangeEventArgs e)
+        {
+
+            errorMessage = string.Empty;
+
+            using var stream = new MemoryStream();
+            await e.File.OpenReadStream().CopyToAsync(stream);
+
+            //make it delimited
+            stream.Position = 0;
+            var contentStringUntrimmed = System.Text.Encoding.UTF8.GetString(stream.ToArray());
+            var contentString = contentStringUntrimmed.Trim(new char[] { '\uFEFF', '\u200B' });//trim the BOM https://en.wikipedia.org/wiki/Byte_Order_Mark
+
+            try
+            {
+                await GetStructuredContent(contentString);
+                await GetSummary(ViewContent);
+                await GetTopicList(ViewContent);
+
+            }
+            catch (Exception exception)
+            {
+                errorMessage = exception.Message + exception.StackTrace;
+                throw;
+            }
+
+        }
+
+
+        public async Task GetStructuredContent(string content)
+        {
+            ViewContent = "";
+            //generate question
+            var promptSettings = GptPromptService.RenderGptPrompt(); //basics only
+            var response = await svc.GetStructuredContent(promptSettings, "UNIT CONTENT: " + content);
+
+            await foreach (var res in response)
+            {
+
+                ViewContent += res;
+
+                StateHasChanged();
+            }
+        }
+
+        public async Task GetSummary(string content)
+        {
+            ViewSummary = "";
+            //generate question
+            var promptSettings = GptPromptService.RenderGptPrompt(); //basics only
+            var response = await svc.GetSummary(promptSettings, "UNIT CONTENT: " + content);
+
+            await foreach (var res in response)
+            {
+
+                ViewSummary += res;
+
+                StateHasChanged();
+            }
+        }
+
+        public async Task GetTopicList(string content)
+        {
+            ViewTopics = "";
+            //generate question
+            var promptSettings = GptPromptService.RenderGptPrompt(); //basics only
+            var response = await svc.GetTopicList(promptSettings, "UNIT CONTENT: " + content);
+
+            await foreach (var res in response)
+            {
+
+                ViewTopics += res;
+
+                StateHasChanged();
+            }
+        }
+    }
+}
---
+++ Enlighten.Study.Core/Services/StudyContentService.cs
@@ -0,0 +1,85 @@
+﻿using Enlighten.Core.Services;
+using Enlighten.Data.Models.Configuration;
+using Enlighten.Gpt.Client.Configuration;
+using Enlighten.Gpt.Client.Services;
+using Enlighten.Gpt.Client.Services.Models;
+
+namespace Enlighten.Study.Core.Services
+{
+    public class StudyContentService
+    {
+        private readonly GptClientSettingsModel _clientSettings;
+
+        public StudyContentService(GptClientSettingsModel clientSettings)
+        {
+            _clientSettings = clientSettings;
+        }
+
+        public async Task<IAsyncEnumerable<string>> GetStructuredContent(GptPromptService.GptPromptRenderModel gptPromptSettings, string textbookContent)
+        {
+            var client = new GptClientService(_clientSettings);
+
+            client.Connect();
+
+            var conversationSettings = InitializeConversation(gptPromptSettings, textbookContent);
+
+            // The bot is requested to generate a short-answer question based on the textbook content
+            var response = await client.StreamResponse(conversationSettings,
+                "From the provided textbook content, extract and return a structured json format, breaking down the content into topics with summaries, subtopics with summaries, and other important details to be used as a knowledge reference for generating questions. Be sure to include the relevant section numbers.");
+            return response;
+        }
+
+
+        public async Task<IAsyncEnumerable<string>> GetSummary(GptPromptService.GptPromptRenderModel gptPromptSettings, string textbookContent)
+        {
+            var client = new GptClientService(_clientSettings);
+
+            client.Connect();
+
+            var conversationSettings = InitializeConversation(gptPromptSettings, textbookContent);
+
+            // The bot is requested to generate a short-answer question based on the textbook content
+            var response = await client.StreamResponse(conversationSettings,
+                "From the provided textbook content, generate a 1 sentence summary");
+            return response;
+        }
+
+        public async Task<IAsyncEnumerable<string>> GetTopicList(GptPromptService.GptPromptRenderModel gptPromptSettings, string textbookContent)
+        {
+            var client = new GptClientService(_clientSettings);
+
+            client.Connect();
+
+            var conversationSettings = InitializeConversation(gptPromptSettings, textbookContent);
+
+            // The bot is requested to generate a short-answer question based on the textbook content
+            var response = await client.StreamResponse(conversationSettings,
+                "From the provided textbook content, generate a topic list in a comma-delimited format");
+            return response;
+        }
+
+        public ConversationSettingsModel InitializeConversation(GptPromptService.GptPromptRenderModel gptPromptSettings, string textbookContent)
+        {
+            var conversationSettings = new ConversationSettingsModel()
+            {
+                SystemMessage = gptPromptSettings.QuizSystemMessage
+            };
+
+            // The user provides content from a geometry textbook
+            var words = textbookContent.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i += 500)
+            {
+                var part = string.Join(" ", words.Skip(i).Take(500));
+                conversationSettings.ExampleDialogs.Add(new ConversationSettingsModel.ExampleDialog()
+                {
+                    UserInput = $"{gptPromptSettings.ContentStart}:{part}",
+                    BotResponse = "READY"
+                });
+            }
+
+            return conversationSettings;
+        }
+
+    }
+}
