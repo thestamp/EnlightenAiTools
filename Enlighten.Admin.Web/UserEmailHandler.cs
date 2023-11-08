41c64a61b76ad44c9728a2c91117820b05f4add3
Jeffrey Lefebvre <j.lefebvre@uwinnipeg.ca>
2023-11-08 11:31:28 -0600

    added github link

---
+++ Enlighten.Admin.Web/UserEmailHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Enlighten.Admin.Web
+{
+    public class UserEmailRequirement : IAuthorizationRequirement { }
+
+    public class UserEmailHandler : AuthorizationHandler<UserEmailRequirement>
+    {
+        private readonly List<string> _allowedUsers;
+
+        public UserEmailHandler(List<string> allowedUsers)
+        {
+            _allowedUsers = allowedUsers;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+            UserEmailRequirement requirement)
+        {
+            var userEmailClaim = context.User?.FindFirst(c => c.Type == "emails");
+            if (userEmailClaim != null && _allowedUsers.Contains(userEmailClaim.Value))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
